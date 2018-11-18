542242dd65dce92f9d1b6324a0811fd1aafee346
Jecket <35532868+Jecket22@users.noreply.github.com>
2018-11-18 12:18:07 +0100

    Uploaded the Source of "Vessel Name Leaker".

---
+++ Vessel Name Leak.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<windows.h>
+#include<shlobj.h>
+
+using namespace std;
+
+int main() {
+	string path; // Defines "path".
+	path = getenv("localappdata"); // Gets %LOCALAPPDATA% Folder and links into "path".
+	// cout << path + "\\DELTARUNE\\filech1_0" << endl; [Tells if the path is correctly, used in mid-development.]
+	ifstream qFile(path + "\\DELTARUNE\\filech1_0"); // Reads File from "Path + Deltarune folder" and links into "qFile".
+	string qLine; // Defines "qLine".
+
+	if (!qFile.good()) { // If file isn't found,
+		cout << "Could not find file..." << endl; // Print this to console.
+		return 1; // Ends Process.
+	}
+
+	int cur_l = 0; // Start Line, used if "cur_l" is later equals to "end_l" to finish process.
+	int end_l = 1; // End Line of what should get read.
+
+	while (!qFile.eof()) {
+		qFile >> qLine; // Copy text to "qLine" from the file "qFile".
+		cout << qLine << " "; // Print to console and add spaces.
+		{
+			if (cur_l == end_l) { // If "cur_l" value matches "end_l"
+				break; // and then finish the program.
+			}
+			cur_l++; // Add a value to "cur_l".
+		}
+	} // Loops if process is still running.
+
+	qFile.close();
+
+	Sleep(5000); // Sleep for 5 Seconds.
+
+	return 0; // Ends Process.
+}
